559217089064512a55bf5fdf74f7b49dba0b8f98
Sim <simeransvirdi@gmail.com>
2018-08-31 05:24:57 +0100

    Updated scores and latex files

--- latex_form.py
+++ latex_form.py
@@ -1,30 +1,38 @@
 def dice_met(inp):
 	first = inp.replace('Average Scores [ ','')
 	split = first.split()
-
-	string = '& '
-	for i in range(1,len(split)):
+	string = '&'
+	for i in range(0,len(split)):
 
 		if split[i]!= ']':
-			string += split[i] + ' & '
-	print(string[:-2]+'\\\\')
+			if split[i] == 'nan':
+				val = 'nan'
+			else:
+				value = split[i].replace(']','')
+				val = float(value)
+				new_val = round(val,2)
+			string += str(new_val) + '&'
+	string = string[:-1]
+	print(string+'\\\\')
 
 def thickness_met(inp):
 	first = inp.replace('Average Scores [ ','')
 	split = first.split()
+	string = '&'
+	for i in range(0,len(split)):
 
-	string = '& '
-	for i in range(1,len(split)):
-
-		if split[i]!= ']':
-			val = float(split[i])
+		if split[i] != ']':
+			value = split[i].replace(']','')
+			val = float(value)
 			new_val = int(round(val,0))
-			string += str(new_val) + ' & '
-	print(string[:-2]+'\\\\')
+			string += str(new_val) + '&'
+	string = string[:-1]
+	print(string+'\\\\')
 
-x = "Average Scores [ 0.316  0.01   0.044  0.071  0.089  0.097  0.082  0.03 ]"
-y = "Average Scores [ 179.839   10.813   25.374   20.678   17.265   36.794    8.95     7.3  ]"
-z = "Average Scores [  96.293  123.936   25.761   86.402   28.225   50.032   12.533   10.734 ]"
+x = "Average Scores [ 0.    0.02  0.08  0.02   nan   nan  0.01]"
+y = "Average Scores [  15.17   35.44   24.8     8.76  112.79    4.5     3.24]"
+z = "Average Scores [ 157.08   25.14   94.23   21.5   100.82   11.1     7.62]"
+
 
 dice_met(x)
 thickness_met(y)
